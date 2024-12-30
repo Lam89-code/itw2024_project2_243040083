de3f57f8e4fb1cdebff3b2a40e4c0ac5d8b8ce9f
Lam89-code <aaallaaamm03@gmail.com>
2024-12-31 02:52:26 +0700

    Banner, Berita, and Footer

--- script.js
+++ script.js
@@ -2,5 +2,33 @@ const menuIcon = document.getElementById("menu-icon");
 const menuList = document.getElementById("menu-list");
 
 menuIcon.addEventListener("click", () => {
-    menuList.classList.toggle("hidden");
+  menuList.classList.toggle("hidden");
+});
+
+new Swiper(".card-wrapper", {
+  loop: true,
+  spaceBetween: 30,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
